a5e360a2a5a5a278ad75be1a39461ac99538a180
Víctor Duplat Tigre <tigrevictorduplat@gmail.com>
2022-03-17 11:08:18 -0300

    Novas Alterações
    Tentando utilizar o .createElement

--- Dia4-AluraFlix/app.js
+++ Dia4-AluraFlix/app.js
@@ -1,12 +1,21 @@
 var listaBanner = [];
+var indexBanner = 0;
 const urlInput = document.getElementById("display-text");
 const divDisplay = document.getElementById("display-banner");
 function estruturaConcat(url) {
+  /*
   const classeBanner = "novo-banner";
   var concatString = "<img class=" + classeBanner + " src=" + url + ">";
   return concatString;
+  */
+  const classeBanner = "novo-banner";
+  var concatString = "class=" + classeBanner + " src=" + url;
+  return concatString;
 }
+
 function AdicionarBanner() {
-  divDisplay.innerHTML = estruturaConcat(urlInput.value);
-  console.log(estruturaConcat(urlInput.value));
+  var newImg = divDisplay.createElement("img");
+  listaBanner.push(urlInput.value);
+  newImg.appendChild(estruturaConcat(listaBanner[indexBanner]));
+  indexBanner++;
 }
